757844c3e9c4013d7c55e77839d8a55e41987f5a
Ryan Djebrouni <ryan.djebrouni@gmail.com>
2023-11-13 01:43:44 -0800

    add Vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Outfit } from 'next/font/google'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 
 const outfit = Outfit({ subsets: ['latin'] })
 
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${outfit.className} bg-neutral-900 text-neutral-300`}>{children}</body>
+      <body className={`${outfit.className} bg-neutral-900 text-neutral-300`}>
+        {children}
+        <Analytics />
+      </body>
     </html>
   )
 }
